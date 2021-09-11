acb99f5b889556fe2374a1d1afe87a11ae5a2ab2
m-GDEV <musaa.ahmed7@gmail.com>
2021-09-10 22:45:31 -0400

    Added reading progress bar.

---
+++ themes/hyde/static/js/progressBar.js
@@ -0,0 +1,13 @@
+// Credit: https://codepen.io/tutsplus/pen/QWGJxPo
+
+let processScroll = () => {
+	let docElem = document.documentElement, 
+		docBody = document.body,
+		scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
+    		scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
+		scrollPercent = scrollTop / scrollBottom * 100 + '%';
+	
+    document.getElementById("progress-bar").style.setProperty("--scrollAmount", scrollPercent);	
+}
+
+document.addEventListener('scroll', processScroll);
