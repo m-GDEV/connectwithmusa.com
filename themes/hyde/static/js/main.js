45547635581f3069b79cf904f096acf899981d20
m-GDEV <musaa.ahmed7@gmail.com>
2021-09-27 20:28:54 -0400

    Added: smooth scroll to top

--- themes/hyde/static/js/main.js
+++ themes/hyde/static/js/main.js
@@ -67,8 +67,12 @@ function topButton() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  //document.body.scrollTop = 0;
+  //document.documentElement.scrollTop = 0;
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth"
+	});
 }
 
 
