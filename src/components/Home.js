65eed1c70d0ff36ce4a0545e4bb1857d092ff5a1
m-GDEV <musaa.ahmed7@gmail.com>
2022-02-12 00:59:36 -0500

    cleaned up dino game

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function About() {
   // dino easter egg toggle
   const [toggled, setToggled] = useState(false);
   // even if user click my name h1, the dino game only shows on screens larger than xl
-  const classChange = toggled ? "hidden xl:block" : "hidden";
+  const classChange = toggled ? "hidden xl:block top-28 right-20 absolute" : "hidden";
 
   return (
     <section id="about" className="bg-gradient-to-t from-g-dark to-g-light">
@@ -33,13 +33,11 @@ export default function About() {
               className="ml-4 inline-flex text-white bg-b-darkpurple rounded-xl border-0 py-2 px-6 focus:outline-none hover:bg-b-brightpurple text-lg md:text-xl sm:whitespace-nowrap">
               About Me
             </Link>
-            <div className={classChange}>
-              <div className="absolute right-20 top-28">
+          </div>
+          <div className={classChange}>
                 <iframe src="https://chrisdothtml.github.io/chrome-dino/" frameborder="0" scrolling="no" width="100%" height="100%" loading="lazy" noreferrer
                 className="w-[620px] h-[200px] z-999 rounded-2xl"></iframe>
                 <p className="text-white text-2xl text-center mt-2">You found an easter 🥚!</p>
-              </div>
-          </div>
           </div>
         </div>
       </div>
