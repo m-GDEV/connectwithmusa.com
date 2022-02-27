d3f1d5a34002315a2be7e823e56c6cab35fc5391
m-GDEV <musaa.ahmed7@gmail.com>
2022-02-27 15:55:52 -0500

    Changed up footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,25 @@
+import { ArrowCircleUpIcon } from "@heroicons/react/solid";
 import React from "react";
+
 
 export default function Contact() {
     let date = new Date().getFullYear();
 
+    const topFunction = () => {
+          window.scrollTo({
+              top: 0,
+              behavior: "smooth"
+          });}
+
     return (
-        <footer className="bg-g-dark bottom-0 left-0 text-center w-full text-white text-sm sm:text-lg pb-4">
-            <p>Built by Musa Ahmed - Since {date} 
-            <nobr> <a href="https://github.com/m-GDEV/connectwithmusa.com"><img src="https://img.shields.io/badge/Source%20Code-white?style=for-the-badge&logo=github&logoColor=black"
-             className="inline-flex ml-2 rounded-md"/> </a></nobr> </p>
+        <footer className="flex flex-col sm:flex-row justify-center items-center bg-g-dark bottom-0 left-0 text-center w-full text-white text-sm sm:text-base pb-4">
+            <p className="mb-2 sm:mb-0">Built by Musa Ahmed · Since {date}</p>
+            <p>
+            <a href="https://github.com/m-GDEV/connectwithmusa.com"><img src="https://img.shields.io/badge/Source%20Code-white?style=for-the-badge&logo=github&logoColor=black"
+            className="inline-flex ml-2 rounded-md"/>&nbsp;&nbsp;·&nbsp;</a>
+            Top
+                <ArrowCircleUpIcon className="w-8 h-8 ml-1 hover:scale-125 transition-all duration-500 inline-flex"
+                onClick={topFunction}/> </p>
         </footer>
       );
 }
