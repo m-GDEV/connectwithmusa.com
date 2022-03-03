a770695b135afd1ad60eb2f8eff284e805f60377
m-GDEV <musaa.ahmed7@gmail.com>
2022-03-03 00:33:04 -0500

    Updated 404 page to not show chess game on mobile, aligned learn more button to bottom of project card

--- src/components/Missing.jsx
+++ src/components/Missing.jsx
@@ -1,20 +1,27 @@
 // using class cus idk how to sleep in functional component
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Missing() {
 
     return (
         <section className=" font-dm-sans italic bg-gradient-to-t from-g-dark to-g-light min-h-screen">
-          <div className="container px-5 py-24 sm:py-48 mx-auto text-center lg:px-40 flex place-items-center flex-col">
+          <div className="container px-5 py-24 sm:py-20 mx-auto text-center lg:px-40 flex place-items-center flex-col">
             <h1 className="text-h-brightgreen text-5xl mb-10 underline">404 This page is missing!</h1>
-            <p className="text-xl mb-5 text-t-darkyellow">
+            <p className="text-xl mb-5 text-t-darkyellow hidden sm:block">
+              Sorry about that! Go <Link to="/" className="underline text-h-brightgreen">Home</Link>
+              <br />
+              <br />
               Here is a fun little chess game you can play against the computer.
               <br />
-              The computer will any of the available moves.
+              The computer will choose any of the available moves.
               <br />
               The game will end when there are no legal moves left.</p>
+              <p className="text-xl mb-5 text-t-darkyellow block sm:hidden">
+              Sorry about that! Visit this page on desktop for a surprise! Or go <Link to="/" className="underline text-h-brightgreen">Home</Link></p>
             <iframe src="https://m-gdev.github.io/chessboardjs/" frameBorder="0" scrolling="no" loading="lazy" noreferrer="true" title="chess board"
-                className="w-[400px] h-[400px] z-999 rounded-2xl inline-flex justify-items-center"></iframe>
+                className="w-[400px] h-[400px] z-999 rounded-2xl justify-items-center hidden sm:inline-flex"></iframe>
+            <p className="text-h-brightgreen mt-2">(Drag the piece to move)</p>
         </div>
         </section>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import { CodeIcon, ExternalLinkIcon } from "@heroicons/react/solid";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { projects } from "../data";
 import { motion } from "framer-motion";
 import  {  sectionDescriptions  }  from  "../data";
@@ -10,6 +10,7 @@ import  {  sectionDescriptions  }  from  "../data";
 export default function Projects() {
 
   const  pageDesc  =  sectionDescriptions[0];
+
 
   useEffect(() => {
     window.scrollTo({top: 0, behavior: "smooth"});
@@ -36,20 +37,20 @@ export default function Projects() {
     
       <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-10 place-items-center break-words">
           {projects.map((projects) => (
-            <div className="transition-all duration-500 w-11/12 sm:w-4/5 md:w-full h-full rounded-[25px] bg-b-darkishpurple pt-3 px-8 sm:px-10 border-[3px] border-br-lightpurple hover:drop-shadow-2xl" key={projects.title}>
+            <div className="flex flex-col text-center transition-all duration-500 w-11/12 sm:w-4/5 md:w-full h-full rounded-[25px] bg-b-darkishpurple pt-3 px-8 sm:px-10 border-[3px] border-br-lightpurple hover:drop-shadow-2xl" key={projects.title}>
               <h2 className="font-dm-sans text-h-brightgreen text-xl sm:text-2xl mb-1 mt-3">
                 {projects.title}
               </h2>
               <p className="font-dm-mono text-t-darkyellow italic tracking-[0.285em] uppercase text-xs sm:text-sm mb-4">
                 {projects.category}
               </p>
-              <p className="font-dm-sans text-sm tracking-wider">
+              <p className="font-dm-sans text-sm tracking-wider mb-4">
               {projects.description}
               </p>
             <motion.a 
             whileHover={{scale:1.1, transition: {duration: 0.4}, }}
             href={projects.link}
-            className="inline-flex rounded-[20px] mt-4 mb-4 font-dm-sans py-1 px-12 text-sm bg-b-darkpurple uppercase">
+            className="inline-flex mt-auto justify-center rounded-[20px] mb-4 font-dm-sans py-1 px-12 text-sm bg-b-darkpurple uppercase">
               Learn More &nbsp;
               <ExternalLinkIcon className="w-5 text-blue-500"/>
             </motion.a>
