57b8391048814c4ef3282ac74a0ec67e841b6d36
m-GDEV <musaa.ahmed7@gmail.com>
2022-03-12 07:18:00 -0500

    no more flex for 404 page

--- src/components/Missing.jsx
+++ src/components/Missing.jsx
@@ -10,22 +10,11 @@ export default function Missing() {
 
     return (
         <section className=" font-dm-sans italic bg-gradient-to-t from-g-dark to-g-light min-h-screen">
-          <div className="container px-5 py-24 sm:py-20 mx-auto text-center lg:px-40 flex flex-col place-items-center">
+          <div className="container px-5 py-24 sm:py-20 mx-auto text-center lg:px-40">
             <h1 className="text-h-brightgreen text-4xl sm:text-5xl mb-10 underline">404 This page is missing!</h1>
-            <p className="text-xl mb-5 text-t-darkyellow hidden sm:block">
+            <p className="text-xl mb-5 text-t-darkyellow">
               Sorry about that! Go <Link to="/" className="underline text-h-brightgreen">Home</Link>
-              <br />
-              <br />
-              Here is a fun little chess game you can play against the computer.
-              <br />
-              The computer will choose any of the available moves.
-              <br />
-              The game will end when there are no legal moves left.</p>
-              <p className="text-xl mb-5 text-t-darkyellow block sm:hidden">
-              Sorry about that! Visit this page on desktop for a surprise! Or go <Link to="/" className="underline text-h-brightgreen">Home</Link></p>
-            <iframe src="https://m-gdev.github.io/chessboardjs/" frameBorder="0" scrolling="no" loading="lazy" noreferrer="true" title="chess board"
-                className="w-[400px] h-[400px] z-999 rounded-2xl justify-items-center hidden sm:inline-flex"></iframe>
-            <p className="text-h-brightgreen mt-2 hidden sm:block">(Drag the piece to move)</p>
+            </p>
         </div>
         </section>
     )
