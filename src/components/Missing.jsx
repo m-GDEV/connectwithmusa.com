02a89ef41f71cd6b7f2f13cea08624af2d3d3d8e
m-GDEV <musaa.ahmed7@gmail.com>
2022-03-12 11:13:29 -0500

    changed home css so that it does not affect 404 page

--- src/components/Missing.jsx
+++ src/components/Missing.jsx
@@ -34,10 +34,9 @@ function Content() {
   return (
     <section className=" font-dm-sans italic bg-gradient-to-t from-g-dark to-g-light min-h-screen">
     <div className="container px-5 py-12 sm:py-20 mx-auto text-center lg:px-40 flex flex-col place-items-center">
-      <h1 className="text-h-brightgreen text-4xl sm:text-5xl mb-10 underline">404 This page is missing!</h1>
+      <h1 className="text-h-brightgreen text-4xl sm:text-5xl mb-5 underline">404 This page is missing!</h1>
       <p className="text-xl mb-5 text-t-darkyellow">
         Sorry about that! Go <Link to="/" className="underline text-h-brightgreen">Home</Link>
-        <br />
         <br />
         Here is the a Wordle, check it out!</p>
       <iframe src="https://reactle.vercel.app/" frameBorder="0" scrolling="no" loading="lazy" noreferrer="true" title="chess board"
