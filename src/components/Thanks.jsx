031edc0700a37fae124192d0763f259ba16a0d29
Musa Ahmed <musaa.ahmed7@gmail.com>
2022-05-22 17:21:47 -0400

    Update Thanks.jsx

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -12,7 +12,7 @@ class Thanks extends React.Component {
     return (
         <section className="text-h-brightgreen font-dm-sans italic bg-gradient-to-t from-g-dark to-g-light min-h-screen">
           <div className="container px-5 py-24 sm:py-48 mx-auto text-center lg:px-40 ">
-            <h1 className="text-5xl">Thank you for sending me a message!<br /><br /> I'll try to respond as soon as possible.</h1>             
+            <h1 className="text-5xl text-white">Thank you for sending me a message!<br /><br /> I'll try to respond as soon as possible.</h1>             
             <p className="text-red-500 text-3xl mt-10">You will be redirected in 5 seconds.</p>
         </div>
         </section>
