5efa641375125da4098c23c0f0d3b2ae63971df7
Musa Ahmed <musaa.ahmed7@gmail.com>
2024-07-13 22:28:47 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 
 export default function Footer() {
-    let date = new Date().getFullYear();
+    //let date = new Date().getFullYear();
 
     const topFunction = () => {
         window.scrollTo({
@@ -13,7 +13,7 @@ export default function Footer() {
 
     return (
         <footer className="flex flex-col sm:flex-row justify-center items-center bg-g-dark bottom-0 left-0 text-center w-full text-white text-sm sm:text-base pb-4">
-            <p className="mb-2 sm:mb-0">Built by Musa Ahmed · Since {date}&nbsp;</p>
+            <p className="mb-2 sm:mb-0">Built by Musa Ahmed · Since 2022&nbsp;</p>
             <p>
                 <a href="https://github.com/m-GDEV/connectwithmusa.com" target="_blank" rel="noreferrer"><img alt="github logo" src="https://img.shields.io/badge/Source%20Code-white?style=for-the-badge&logo=github&logoColor=black"
                     className="inline-flex ml-2 rounded-md"/>&nbsp;&nbsp;·&nbsp;</a>
